dfa964dbecd608b411f8f8a7a97e3d7fb61c688f
valenporta <133925090+valenporta@users.noreply.github.com>
2026-03-27 11:42:53 -0300

    Actualizo frontend url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
